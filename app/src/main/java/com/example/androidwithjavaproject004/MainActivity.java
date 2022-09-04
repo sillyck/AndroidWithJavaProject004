9c47234f3d7f0e437441040f788b5e58e647ff48
Jordi Ribellas <ribellas2002@gmail.com>
2022-09-04 18:34:08 +0200

    onClick switch1 and switch2

--- app/src/main/java/com/example/androidwithjavaproject004/MainActivity.java
+++ app/src/main/java/com/example/androidwithjavaproject004/MainActivity.java
@@ -22,9 +22,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void verificar(View V) {
         if (switch1.isChecked()) {
-            Toast.makeText(this, "Els datos estan activats", Toast.LENGTH_SHORT).show();
-        } else if (switch2.isChecked()) {
-            Toast.makeText(this, "La wifi esta activada", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Datos activats", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Datos desactivats", Toast.LENGTH_SHORT).show();
+        }
+
+        if (switch2.isChecked()) {
+            Toast.makeText(this, "Wifi activada", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Wifi desactivada", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void verificarDatos(View V){
+        if (switch1.isChecked()){
+            Toast.makeText(this, "Datos activats", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Datos desactivats", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void verificarWifi(View V){
+        if (switch2.isChecked()){
+            Toast.makeText(this, "Wifi activats", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Wifi desactivats", Toast.LENGTH_SHORT).show();
         }
     }
 }
