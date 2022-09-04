88944dd63262fa7604789d99de222dc7e5e3278a
Jordi Ribellas <ribellas2002@gmail.com>
2022-09-04 18:28:14 +0200

    Switch Project

--- app/src/main/java/com/example/androidwithjavaproject004/MainActivity.java
+++ app/src/main/java/com/example/androidwithjavaproject004/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.androidwithjavaproject004;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch switch1, switch2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switch1 = findViewById(R.id.switch1);
+        switch2 = findViewById(R.id.switch2);
+    }
+
+    public void verificar(View V) {
+        if (switch1.isChecked()) {
+            Toast.makeText(this, "Els datos estan activats", Toast.LENGTH_SHORT).show();
+        } else if (switch2.isChecked()) {
+            Toast.makeText(this, "La wifi esta activada", Toast.LENGTH_SHORT).show();
+        }
     }
 }
